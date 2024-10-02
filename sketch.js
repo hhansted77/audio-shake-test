60e83a751bd1fbfd93a3877522017aeae2ba3916
Hannah Hansted <hmh22@fsu.edu>
2024-10-02 09:51:52 -0400

    switch to mp3

--- sketch.js
+++ sketch.js
@@ -4,18 +4,18 @@ let boxGray = 125;
 let value = 0;
 let threshold = 50;
 
-//let first;
-//let second;
-//let third;
-//let fourth;
+let first;
+let second;
+let third;
+let fourth;
 
 
-//function preload() { //sounds to be played
-  //first = loadSound("libraries/stop that.wav");
-  //second = loadSound("libraries/stop it.wav");
-  //third = loadSound("libraries/put me down.wav");
-  //fourth = loadSound("libraries/nonono.wav");
-  //}
+function preload() { //sounds to be played
+  first = loadSound("assets/stop that.mp3");
+  second = loadSound("assets/stop it.mp3");
+  third = loadSound("assets/put me down.mp3");
+  fourth = loadSound("assets/nonono.mp3");
+  }
   
   function setup() {
     fill(value);
@@ -27,16 +27,16 @@ let threshold = 50;
 
 
   function deviceShaken() {  //this is supposed to be the randomizer
-    //let rndm = random(1, 100);
-    //if (rndm <= 25) {
-      //first.play();
-    //} else if (26 <= rndm <= 50){
-      ///second.play();
-    //} else if (51 <= rndm <= 75){
-      //third.play();
-    //}else {
-      //fourth.play();
-    //}
+    let rndm = random(1, 100);
+    if (rndm <= 25) {
+      first.play();
+    } else if (26 <= rndm <= 50){
+      second.play();
+    } else if (51 <= rndm <= 75){
+      third.play();
+    }else {
+      fourth.play();
+    }
 
 value = value + 5;
 if (value > 255){
